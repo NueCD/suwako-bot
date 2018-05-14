b937a5b2b7196eb0a11e307dc7beba780d338a6a
root <root@puppet-s1.example.com>
2018-05-14 10:47:35 +0200

    Fixed working runscript.,

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # Check python version
 PVERSION=$(python -c "import sys; print(sys.version_info[0])")
 PYTHON='python'
+WDIR="$(pwd)"
 
 if ! [[ $PVERSION = '3' ]]; then
 	if [[ $PVERSION = '2' ]]; then
@@ -16,14 +17,16 @@ if ! [[ $PVERSION = '3' ]]; then
 	fi
 fi
 
+# Show help text
 if [[ $* = *'-h'* || $* = *'--help'* ]]; then
-	echo "Usage: $(tput bold)run.sh$(tput sgr0) [-v|--virtualenv] [--setup]"
-	echo "    $(tput bold)-v, --virtualenv$(tput sgr0) Run bot inside virtualenvironment."
-	echo "             $(tput bold)--setup$(tput sgr0) Install dependencies."
+	echo "Usage: $(tput bold)run.sh$(tput sgr0) [--setup] [-v|--virtualenv]"
 	echo "          $(tput bold)-h, --help$(tput sgr0) Show this message."
+	echo "             $(tput bold)--setup$(tput sgr0) Install dependencies."
+	echo "    $(tput bold)-v, --virtualenv$(tput sgr0) Run bot inside virtualenvironment."
 	exit
 fi
 
+# Virtualenv mode.
 if [[ $* = *'-v'* || $* = *'--virtualenv'* ]]; then
 	# Check if virtualenv is installed
 	if ! [[ $(command -v virtualenv) ]]; then
@@ -33,21 +36,27 @@ if [[ $* = *'-v'* || $* = *'--virtualenv'* ]]; then
 	
 	# Prepare virtualenv
 	if [[ $* = *'--setup'* ]]; then
-		virtualenv .
-		source bin/activate
-		pip3.6 install -r requirements.txt
+		virtualenv -p $PYTHON .
+		. $WDIR/bin/activate
+		pip install -r requirements.txt
 		deactivate
 		echo 'Virtualenv is done.'
 		exit
 	fi
 	
 	# Run virtualenv
-	if ! [[ -f "bin/activate" ]]; then
+	if ! [[ -f "./bin/activate" ]]; then
 		echo 'Virtualenv is not set up. Run "suwkao.sh -v --setup" to set up.'
 		exit
 	fi
-	source bin/activate
+	. $WDIR/bin/activate
+	echo 'Starting Suwako in virtualenv mode.'
+	python suwako.py
+	deactivate
+	echo 'Exiting...'
+	exit
 
+# Global mode setup
 elif [[ $* = *'--setup'* ]]; then
 	if ! [[ $(command -v pip) ]]; then
 		echo 'Pip is not installed. Exiting...'
@@ -55,6 +64,10 @@ elif [[ $* = *'--setup'* ]]; then
 	fi
 	
 	pip install -r requirements.txt
-fi
 
-eval $PYTHON suwako.py
+# Global mode
+else
+	echo 'Starting Suwako in global mode.'
+	eval $PYTHON suwako.py
+	echo 'Exiting...'
+fi
